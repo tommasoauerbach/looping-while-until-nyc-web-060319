b6c475dcda5c039b3044a619caab4189b4ee29c6
tommaso auerbach <tsa302@nyu.edu>
2019-06-13 17:30:33 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
-  counter = 0
-  while counter < 10
-    puts "Wingardium Leviosa"
-    counter += 1
-  end
+counter = 6
+while counter < 10
+  puts "Wingardium Leviosa"
+  counter += 1
+end
 end
